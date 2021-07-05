6196463afae296359c7773f469d1295433bb852b
Jhonata L. Rambo <jhonatan.ramboo@hotmail.com>
2021-07-05 18:36:09 -0300

    test: create inference routes

---
+++ src/main/routes/inference-routes.test.ts
@@ -0,0 +1,30 @@
+import request from 'supertest'
+import { MongoHelper } from '../../infra/db/mongodb/helpers/mongo-helper'
+import app from '../config/app'
+
+describe('Inference Routes', () => {
+  beforeAll(async () => {
+    await MongoHelper.connect(process.env.MONGO_URL)
+  })
+
+  beforeEach(async () => {
+    const accountCollection = MongoHelper.getCollection('accounts')
+    await accountCollection.deleteMany({})
+  })
+
+  afterAll(async () => {
+    await MongoHelper.disconnect()
+  })
+
+  test('Should return an account on success', async () => {
+    await request(app)
+      .post('/api/inference')
+      .send({
+        normal_image: 'http:test',
+        inferred_image: 'http:test',
+        inference: { test: 2 },
+        created_at: '12/12/1222'
+      })
+      .expect(200)
+  })
+})
