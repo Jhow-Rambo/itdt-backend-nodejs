c5ce25b648fb9646958c7d0fe5c1ca73256c1c4f
Jhonata L. Rambo <jhonatan.ramboo@hotmail.com>
2021-07-05 01:40:11 -0300

    test: ensure that return 400 if no normal image is provided

---
+++ src/presentation/controllers/inference/inference.spec.ts
@@ -0,0 +1,49 @@
+import { MissingParamError } from '../../errors'
+import { InferenceController } from './inference'
+import { AddInference, InferenceModel, AddInferenceModel } from './inference-protocols'
+
+const makeAddInferenceStub = (): AddInference => {
+  class AddAccountStub implements AddInference {
+    async add (inference: AddInferenceModel): Promise<InferenceModel> {
+      const fakeInference = {
+        id: 'valid_id',
+        normal_image: 'valid_normal_image',
+        inferred_image: 'valid_inferred_image',
+        inference: 'valid_inference',
+        created_at: 'valid_data'
+      }
+      return new Promise(resolve => resolve(fakeInference))
+    }
+  }
+  return new AddAccountStub()
+}
+
+interface SutTypes {
+  sut: InferenceController
+  addInferenceStub: AddInference
+}
+
+const makeSut = (): SutTypes => {
+  const addInferenceStub = makeAddInferenceStub()
+  const sut = new InferenceController(addInferenceStub)
+  return {
+    sut,
+    addInferenceStub
+  }
+}
+
+describe('Inference Controller', () => {
+  test('Should return 400 if no normal image is provided', async () => {
+    const { sut } = makeSut()
+    const httpRequest = {
+      body: {
+        inferred_image: 'any_inferred_image',
+        inference: 'any_inference',
+        created_at: 'any_data'
+      }
+    }
+    const httpResponse = await sut.handle(httpRequest)
+    expect(httpResponse.statusCode).toBe(400)
+    expect(httpResponse.body).toEqual(new MissingParamError('normal_image'))
+  })
+})
