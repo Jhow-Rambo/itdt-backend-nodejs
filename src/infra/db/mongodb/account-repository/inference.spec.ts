437b29faa44db1f7d4018aa0a6536ebb54970b83
Jhonata L. Rambo <jhonatan.ramboo@hotmail.com>
2021-07-05 16:19:15 -0300

    test: ensure that mongodb save inferrenceData and returns the result

---
+++ src/infra/db/mongodb/account-repository/inference.spec.ts
@@ -0,0 +1,37 @@
+import { MongoHelper } from '../helpers/mongo-helper'
+import { InferenceMongoRepository } from './inference'
+
+const makeSut = (): InferenceMongoRepository => {
+  return new InferenceMongoRepository()
+}
+
+describe('Inference Mongo Repository', () => {
+  beforeAll(async () => {
+    await MongoHelper.connect(process.env.MONGO_URL)
+  })
+
+  beforeEach(async () => {
+    const inferenceCollection = MongoHelper.getCollection('inferences')
+    await inferenceCollection.deleteMany({})
+  })
+
+  afterAll(async () => {
+    await MongoHelper.disconnect()
+  })
+
+  test('Sould return an inference on success', async () => {
+    const sut = makeSut()
+    const inference = await sut.add({
+      normal_image: 'valid_normal_image',
+      inferred_image: 'valid_inferred_image',
+      inference: 'valid_inference',
+      created_at: 'valid_date'
+    })
+    expect(inference).toBeTruthy()
+    expect(inference.id).toBeTruthy()
+    expect(inference.normal_image).toBe('valid_normal_image')
+    expect(inference.inferred_image).toBe('valid_inferred_image')
+    expect(inference.inference).toBe('valid_inference')
+    expect(inference.created_at).toBe('valid_date')
+  })
+})
