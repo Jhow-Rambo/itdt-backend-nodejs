4c5b8ef5a0451294c603816875e775fbf8c793f3
Jhonata L. Rambo <jhonatan.ramboo@hotmail.com>
2021-07-06 17:00:31 -0300

    feat: ensure that GetInferenceController return 200 if valid inference is provided

--- src/presentation/protocols/controller.ts
+++ src/presentation/protocols/controller.ts
@@ -3,3 +3,7 @@ import { HttpRequest, HttpResponse } from './http'
 export interface Controller {
   handle (httpRequest: HttpRequest): Promise<HttpResponse>
 }
+
+export interface InferenceGet {
+  get (inference: any): Promise<HttpResponse>
+}
